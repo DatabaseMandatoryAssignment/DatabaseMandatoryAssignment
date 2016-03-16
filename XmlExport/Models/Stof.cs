e0df0fac1b141430ce2c21f4bbb6199d04713701
Daniel <danielwinther@hotmail.dk>
2016-03-16 15:29:21 +0100

    latest xml exporter

---
+++ XmlExport/Models/Stof.cs
@@ -0,0 +1,28 @@
+namespace XmlExport.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Stof")]
+    public partial class Stof
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Stof()
+        {
+            Ambient = new HashSet<Ambient>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int StofId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string StofNavn { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Ambient> Ambient { get; set; }
+    }
+}
---
+++ XmlExport/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using XmlExport.Models;
+
+namespace XmlExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            // //OpstillingId[not(. = following::OpstillingId/.)]
+            const string Path = @"C:\Users\SanneWinther\Desktop\datamatiker4.semester\database\MandatoryAssignment\XmlExport\";
+            document.Load(Path + "Ambient.xml");
+            XmlNode root = document.DocumentElement;
+
+            XmlNodeList nodes = root.SelectNodes("//Data");
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine("--- Start! ---");
+            stopwatch.Start();
+            foreach (XmlNode item in nodes)
+            {
+                using (DataContext dataContext = new DataContext())
+                {
+                    dataContext.Ambient.Add(new Ambient()
+                    {
+                        DatoMaerke = Convert.ToDateTime(item.SelectSingleNode("DatoMaerke").InnerText),
+                        MaaleStedId = Convert.ToInt32(item.SelectSingleNode("MaaleStedId").InnerText),
+                        OpstillingId = Convert.ToInt32(item.SelectSingleNode("OpstillingId").InnerText),
+                        StofId = Convert.ToInt32(item.SelectSingleNode("StofId").InnerText),
+                        Resultat = Convert.ToDouble(item.SelectSingleNode("Resultat")?.InnerText.Replace(".", ",")),
+                        EnhedId = Convert.ToInt32(item.SelectSingleNode("EnhedId").InnerText),
+                        UdstyrId = Convert.ToInt32(item.SelectSingleNode("UdstyrId").InnerText),
+                        Easting32 = Convert.ToInt32(item.SelectSingleNode("Easting_32").InnerText),
+                        Northing32 = Convert.ToInt32(item.SelectSingleNode("Northing_32").InnerText)
+                    });
+                    dataContext.SaveChanges();
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine("--- Done! --- (" + stopwatch.Elapsed.ToString("g") + ")");
+            Console.Read();
+        }
+    }
+}
