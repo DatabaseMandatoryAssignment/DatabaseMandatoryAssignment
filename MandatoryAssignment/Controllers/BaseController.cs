e313addc65c0c8f04d5e6980c765fd65320655ad
Daniel <danielwinther@hotmail.dk>
2016-03-31 14:06:24 +0200

    log

--- MandatoryAssignment/Controllers/BaseController.cs
+++ MandatoryAssignment/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,9 +8,19 @@ using MandatoryAssignment.Models;
 
 namespace MandatoryAssignment.Controllers
 {
-    public abstract class BaseController : Controller
+    public class BaseController : Controller
     {
+        protected const string Message = "MESSAGE";
+        protected const string Notice = "NOTICE";
+        protected const string Warning = "WARNING";
+        protected const string Error = "ERROR";
+
         protected readonly DataContextTable DataContextTable = new DataContextTable();
         protected readonly DataContextView DataContextView = new DataContextView();
+
+        protected void Log(string message, string type)
+        {
+            Trace.WriteLine(message, type + " - [" + DateTime.Now + "]");
+        }
     }
 }
