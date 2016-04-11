73df4a4fea8dfe5a4f56ba843e92b1108cf00a10
Daniel <danielwinther@hotmail.dk>
2016-04-11 17:18:06 +0200

    ambient

---
+++ MandatoryAssignment2/Models/MongoDbAmbient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace MandatoryAssignment2.Models
+{
+
+    public class MongoDbAmbient
+    {
+        public ObjectId _id { get; set; }
+        public DateTime DatoMaerke { get; set; }
+        public string MaaleSted { get; set; }
+        public string Opstilling { get; set; }
+        public string Stof { get; set; }
+        public int Resultat { get; set; }
+        public string Enhed { get; set; }
+        public string Udstyr { get; set; }
+        public int Easting32 { get; set; }
+        public int Northing32 { get; set; }
+    }
+
+}
