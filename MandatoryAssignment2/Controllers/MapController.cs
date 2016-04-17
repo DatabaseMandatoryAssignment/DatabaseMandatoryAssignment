2f809ca4cc9173c1587bc5f398d4c0227595f78c
Daniel <danielwinther@hotmail.dk>
2016-04-17 10:33:09 +0200

    maps

---
+++ MandatoryAssignment2/Controllers/MapController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MandatoryAssignment2.Models;
+
+namespace MandatoryAssignment2.Controllers
+{
+    public class MapController : BaseController
+    {
+        public ActionResult Index()
+        {
+            List<double?> number = new List<double?>();
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "H.C. Andersen Boulevard, Skur" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Jagtvej, Skur" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Anholt, Radar stationen" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Føllesbjerg" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Odense Gade,Skur 5, Odense Albanigade" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Århus gade (3)" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "H.C.Ørsted Inst." select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Odense Tag (2)" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Aalborg Tag ny" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Ulborg" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Risø" select q.Resultat).Average() ?? 0);
+            number.Add((from q in DataContextView.AmbientView where q.Maalested == "Aarhus Botanisk" select q.Resultat).Average() ?? 0);
+
+            return View(number);
+        }
+    }
+}
